39a673b8c88337122a98fad11d953fee309e1fcf
Ayhan <ayhankuru@yandex.com.tr>
2019-08-01 17:58:25 +0300

    Cache translation

--- repositories/translationRepository.ts
+++ repositories/translationRepository.ts
@@ -1,12 +1,19 @@
 import LocaleString from "../entities/LocaleString";
 
+const locale_cache = new Map();
+
 async function getTranslation(
   locale: LocaleString
 ): Promise<Record<string, string>> {
+    
+  if (typeof window !== "undefined" && locale_cache.has(locale)) {
+    return locale_cache.get(locale)
+  } 
+   
   if (typeof window !== "undefined") {
     const response = await fetch(`/static/translation/${locale}.json`);
     const json = await response.json();
-
+    locale_cache.set(locale, json)
     return json;
   }
 
