5b98fcb9c802a72a760e701c3ea59b04f858539f
Kohei Asai <me@axross.io>
2019-12-07 01:00:41 -0800

    Preconnecting URLs (#73)

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ export const NO_LOCALE_MATTER_PATHS = new Set(["/robots.txt", "/sitemap.xml"]);
 
 export const NO_LOCALE_FALLBACK_PATHS = new Set(["/posts/feed.xml"]);
 
-export const DNS_PREFETCH_URLS = new Set([
+export const PRECONNECT_URLS = new Set([
   "https://images.ctfassets.net",
   "https://www.google.ca",
   "https://www.google.com",
