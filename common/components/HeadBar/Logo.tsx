e8bfe3f8700b1c08966517ecea3b330e239b24df
Kohei Asai <yo@kohei.dev>
2020-01-09 11:44:14 -0800

    Make <Logo> Responsible

--- common/components/HeadBar/Logo.tsx
+++ common/components/HeadBar/Logo.tsx
@@ -1,29 +1,40 @@
 import styled from "@emotion/styled";
 import * as React from "react";
+import { MOBILE } from "../../constant/mediaQuery";
 import ThemedColor from "../../types/ThemedColor";
-import Text from "../Text";
+import Text, { TextType } from "../Text";
 
 export default function Logo() {
   return (
     <Root>
       <Image />
 
-      <Text color={ThemedColor.emphasizedForeground}>kohei.dev</Text>
+      <Text type={TextType.logo} color={ThemedColor.emphasizedForeground}>kohei.dev</Text>
     </Root>
   );
 }
 
 const Root = styled.h1`
-  display: inline-grid;
-  grid-template-columns: 24px auto;
+  display: grid;
+  grid-template-columns: 48px auto;
   align-items: center;
-  column-gap: 8px;
+  column-gap: 16px;
+
+  ${MOBILE} {
+    grid-template-columns: 24px auto;
+    column-gap: 8px;
+  }
 `;
 
 const Image = styled.span`
-  display: inline-block;
-  width: 24px;
-  height: 24px;
+  display: block;
+  width: 48px;
+  height: 48px;
   background: center/contain no-repeat url("/profile.jpg");
   border-radius: 50%;
+
+  ${MOBILE} {
+    width: 24px;
+    height: 24px;
+  }
 `;
