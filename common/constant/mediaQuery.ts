5604d15c2899f3b0a4e0b50f90ed4bc1d1cb7ed7
Kohei Asai <yo@kohei.dev>
2020-01-11 15:54:21 -0800

    Change Breakpoint from 960px to 768px (#123)

--- common/constant/mediaQuery.ts
+++ common/constant/mediaQuery.ts
@@ -1,3 +1,3 @@
-export const MOBILE = "@media screen and (max-width: 960px)";
+export const MOBILE = "@media screen and (max-width: 768px)";
 
 export const DARK_MODE = "@media (prefers-color-scheme: dark)";
