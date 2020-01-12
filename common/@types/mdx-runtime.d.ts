526efde2c46fc1e7e9b4da9511bba34b55351301
Kohei Asai <yo@kohei.dev>
2020-01-11 16:38:58 -0800

    Modify tsconfig.json

--- common/@types/mdx-runtime.d.ts
+++ common/@types/mdx-runtime.d.ts
@@ -1,41 +1,39 @@
-declare module '@mdx-js/runtime' {
-  import * as React from 'react';
+import * as React from 'react';
 
-  type ComponentType =
-    | 'p'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
-    | 'thematicBreak'
-    | 'blockquote'
-    | 'ul'
-    | 'ol'
-    | 'li'
-    | 'table'
-    | 'tr'
-    | 'td'
-    | 'pre'
-    | 'code'
-    | 'em'
-    | 'strong'
-    | 'delete'
-    | 'inlineCode'
-    | 'hr'
-    | 'a'
-    | 'img';
+type ComponentType =
+  | 'p'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'thematicBreak'
+  | 'blockquote'
+  | 'ul'
+  | 'ol'
+  | 'li'
+  | 'table'
+  | 'tr'
+  | 'td'
+  | 'pre'
+  | 'code'
+  | 'em'
+  | 'strong'
+  | 'delete'
+  | 'inlineCode'
+  | 'hr'
+  | 'a'
+  | 'img';
 
-  export type Components = {
-    [key in ComponentType]?: React.ComponentType<any>
-  }
-
-  export interface MDXProps {
-    components: Components;
-    scope?: Record<string, any>;
-    children: React.ReactNode;
-  }
-
-  export default class MDX extends React.Component<MDXProps> {}
+export type Components = {
+  [key in ComponentType]?: React.ComponentType<any>
 }
+
+export interface MDXProps {
+  components: Components;
+  scope?: Record<string, any>;
+  children: React.ReactNode;
+}
+
+export default class MDX extends React.Component<MDXProps> {}
