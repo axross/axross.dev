4a77d4fe0d436f51073a2b664c41390ef08c3a55
Kohei Asai <yo@kohei.dev>
2020-01-14 15:07:03 -0800

    Set Storybook Background (#133)

--- .storybook/globalStyleDecorator.js
+++ .storybook/globalStyleDecorator.js
@@ -1,3 +1,4 @@
+import { css, Global } from "@emotion/core";
 import { makeDecorator } from '@storybook/addons';
 import * as React from "react";
 import GlobalStyle from '../common/components/GlobalStyle';
@@ -15,8 +16,25 @@ export default makeDecorator({
       <>
         <GlobalStyle />
 
+        {/* override */}
+        <Global styles={OVERRIDE_GLOBAL_STYLE} />
+
         {storyFn(context)}
       </>
     );
   }
 })
+
+const OVERRIDE_GLOBAL_STYLE = css`
+  html {
+    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAABlBMVEX////Y2NgXsaIHAAAAE0lEQVR4AWNgYPj/n4oElU1jAADtvT/BfzVwSgAAAABJRU5ErkJggg==");
+
+    @media (prefers-color-scheme: dark) {
+      background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAQMAAABJtOi3AAAABlBMVEUAAAAnJyfnqu6kAAAAE0lEQVR4AWNgYPj/n4oElU1jAADtvT/BfzVwSgAAAABJRU5ErkJggg==");
+    }
+  }
+
+  body {
+    margin: 8px;
+  }
+`;
