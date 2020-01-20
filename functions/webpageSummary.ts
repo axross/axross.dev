0fd87fe6cf9f4bd69ad01dd22c72bc0faa915340
Kohei Asai <yo@kohei.dev>
2020-01-19 22:16:38 -0800

    Let Webpage Summary Respond Cache-Control

--- functions/webpageSummary.ts
+++ functions/webpageSummary.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import * as crypto from "crypto";
 import ScrapingWebpageSummaryRepository from "./repositories/ScrapingWebpageSummaryRepository";
 
 export async function handler({ httpMethod, queryStringParameters }: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
@@ -18,7 +19,9 @@ export async function handler({ httpMethod, queryStringParameters }: APIGatewayP
     return {
       statusCode: 200,
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json; charset=UTF-8",
+        "cache-control": `public, max-age=${60 * 60 * 24 * 90}`,
+        "etag": crypto.createHash("sha1").update(`${webpageSummary.title}${webpageSummary.description}`).digest("hex"),
       },
       body: JSON.stringify(webpageSummary),
     };
