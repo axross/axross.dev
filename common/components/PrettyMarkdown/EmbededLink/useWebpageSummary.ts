09b127d39d197ffecf8b4cbb2853638c81ff2bb5
Kohei Asai <yo@kohei.dev>
2020-01-19 16:10:44 -0800

    Add tests for useWebpageSummary()

---
+++ common/components/PrettyMarkdown/EmbededLink/useWebpageSummary.ts
@@ -0,0 +1,16 @@
+import * as React from "react";
+import RepositoryContext from "../../../contexts/RepositoryContext";
+import WebpageSummary from "../../../entities/WebpageSummary";
+
+export default function useWebpageSummary(url: URL): [WebpageSummary | null, boolean] {
+  const { webpageSummaryRepository } = React.useContext(RepositoryContext);
+  const [[webpageSummary, isLoading], set] = React.useState<[WebpageSummary | null, boolean]>([null, true]);
+
+  React.useEffect(() => {
+    webpageSummaryRepository.getByURL(url)
+      .then(webpageSummary => set([webpageSummary, false]))
+      .catch(() => set([null, false]));
+  }, [url]);
+
+  return [webpageSummary, isLoading];
+}
