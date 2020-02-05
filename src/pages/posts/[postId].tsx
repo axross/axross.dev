9e60128a885a96da0c02cca951803bcd48cc87cd
Kohei Asai <yo@kohei.dev>
2020-02-05 14:28:08 -0800

    Prerender meta tags on blog post pages (#181)

--- src/pages/posts/[postId].tsx
+++ src/pages/posts/[postId].tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import * as Contentful from "contentful";
 import { useRouter } from "next/router";
 import * as React from "react";
 import HeadBar from "../../components/HeadBar";
@@ -9,16 +10,27 @@ import Head from "../../components/pages/posts/[postId]/Head";
 import useBlogPost from "../../components/pages/posts/[postId]/useBlogPost";
 import useLoggingPageView from "../../components/pages/posts/[postId]/useLoggingPageView";
 import { MOBILE } from "../../constant/mediaQuery";
+import BlogPost from "../../entities/BlogPost";
+import { NextPageContext } from "next";
+import ContentfulBlogPostApi from "../../repositories/ContentfulBlogPostApi";
 
-export default function BlogPostPage() {
+interface Props {
+  blogPostJSON?: Record<string, any>;
+}
+
+export default function BlogPostPage({ blogPostJSON }: Props) {
   const router = useRouter();
   const blogPostId = router.query.postId;
-  const [blogPost, isBlogPostLoading] = useBlogPost(blogPostId as any);
+  const deserializedBlogPost = blogPostJSON ? deserializeBlogPost(blogPostJSON) : null;
+  const [blogPost, isBlogPostLoading] = useBlogPost(blogPostId.toString());
   useLoggingPageView(blogPost, isBlogPostLoading);
 
   return (
     <>
-      <Head blogPost={blogPost} blogPostLoading={isBlogPostLoading} />
+      <Head
+        blogPost={blogPost ?? deserializedBlogPost}
+        blogPostLoading={deserializedBlogPost ? false : isBlogPostLoading}
+      />
 
       <Root>
         <_HeadBar />
@@ -33,6 +45,46 @@ export default function BlogPostPage() {
       </Root>
     </>
   );
+}
+
+BlogPostPage.getInitialProps = async ({ query, asPath, req }: NextPageContext): Promise<Props> => {
+  if (req) {
+    const url = new URL(asPath!, process.env.ORIGIN);
+    const currentLocale = url.searchParams.get("hl")!;
+    const contentful = Contentful.createClient({
+      // do not specify hosts and preview access token for safety
+      space: process.env.CONTENTFUL_SPACE!,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+    });
+    const blogPostApi = new ContentfulBlogPostApi(contentful);
+    const blogPost = await blogPostApi.getByIdAndLocale(query.postId.toString(), currentLocale);
+
+    return { blogPostJSON: serializeBlogPost(blogPost) };
+  }
+
+  return {};
+};
+
+function serializeBlogPost(blogPost: BlogPost): Record<string, any> {
+  return {
+    id: blogPost.id,
+    createdAt: blogPost.createdAt.toJSON(),
+    lastModifiedAt: blogPost.lastModifiedAt.toJSON(),
+    title: blogPost.title,
+    summary: blogPost.summary,
+    body: blogPost.body,
+  };
+}
+
+function deserializeBlogPost(json: Record<string, any>): BlogPost {
+  return {
+    id: json.id,
+    createdAt: new Date(json.createdAt),
+    lastModifiedAt: new Date(json.lastModifiedAt),
+    title: json.title,
+    summary: json.summary,
+    body: json.body,
+  };
 }
 
 const Root = styled.div`
