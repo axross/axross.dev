7a84da9a39ad1bae6baffd9c46f6be784cb48e24
Kohei Asai <yo@kohei.dev>
2020-02-05 18:29:00 -0800

    Delete TransplationContext and UrlContext
    they are not used

--- src/contexts/TranslationContext.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-interface TranslationContextData {
-  translation: Record<string, string> | null;
-  isLoading: boolean;
-}
-
-export default React.createContext<TranslationContextData>(null as any);
