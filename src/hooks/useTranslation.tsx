f145e346ca5adf767339dae2e070b776d8466ed8
Kohei Asai <yo@kohei.dev>
2020-02-11 17:04:48 -0800

    Modify useTranslation()’s memoization straregy

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -3,13 +3,16 @@ import * as React from "react";
 import dictionary from "../dictionary";
 import useLocale from "./useLocale";
 
-export default function useTranslation(key: string, args?: Record<string, any>): string {
+export default function useTranslation(key: string, args: Record<string, string | number> = {}): string {
   const { currentLocale } = useLocale();
 
+  // TODO:
+  // bug possiblity:
+  // JSON.stringify(args) returns different values if the args' key order is not the same
   return React.useMemo(() => {
     if (!dictionary[key]) throw new Error(`translation (key: ${key}, locale: ${currentLocale}) is not found.`);
     if (!dictionary[key][currentLocale]) throw new Error(`translation (key: ${key}, locale: ${currentLocale}) is not found.`);
 
     return new IntlMessageFormat(dictionary[key][currentLocale]).format(args);
-  }, [key, currentLocale]);
+  }, [key, currentLocale, JSON.stringify(args)]);
 }
