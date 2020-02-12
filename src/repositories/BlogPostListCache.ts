1fc97de1843ef2dcbc3179a5d244e1bf9094dd46
Kohei Asai <yo@kohei.dev>
2020-02-12 15:29:01 -0800

    Delete repository classes no longer used

--- src/repositories/BlogPostListCache.ts
+++
@@ -1,8 +0,0 @@
-import BlogPost from "../entities/BlogPost";
-import LocaleString from "../entities/LocaleString";
-
-export default interface BlogPostListCache {
-  has(locale: LocaleString): boolean;
-  get(locale: LocaleString): BlogPost[];
-  set(locale: LocaleString, blogPosts: BlogPost[]): void;
-}
