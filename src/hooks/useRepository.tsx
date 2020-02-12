b7f0d8b6b70fd1d2525c19a45a48cdedc6a87c57
Kohei Asai <yo@kohei.dev>
2020-02-12 15:30:47 -0800

    Update injected repositories

--- src/hooks/useRepository.tsx
+++ src/hooks/useRepository.tsx
@@ -1,24 +1,16 @@
 import * as React from "react";
-import BioCache from "../repositories/BioCache";
-import BioApi from "../repositories/BioApi";
-import BlogPostCache from "../repositories/BlogPostCache";
-import BlogPostListCache from "../repositories/BlogPostListCache";
-import BlogPostApi from "../repositories/BlogPostApi";
-import LocaleApi from "../repositories/LocaleApi";
-import WebpageSummaryApi from "../repositories/WebpageSummaryApi";
-import WebsitePurposeCache from "../repositories/WebsitePurposeCache";
-import WebsitePurposeApi from "../repositories/WebsitePurposeApi";
+import GetBio from "../repositories/bio/GetBio";
+import GetAllBlogPosts from "../repositories/blogPost/GetAllBlogPosts";
+import GetBlogPost from "../repositories/blogPost/GetBlogPost";
+import GetWebpageSummary from "../repositories/webpageSummary/GetWebpageSummary";
+import GetWebsitePurpose from "../repositories/websitePurpose/GetWebsitePurpose";
 
-interface Repositories {
-  bioApi: BioApi;
-  bioCache: BioCache;
-  blogPostApi: BlogPostApi;
-  blogPostCache: BlogPostCache;
-  blogPostListCache: BlogPostListCache;
-  localeApi: LocaleApi;
-  websitePurposeApi: WebsitePurposeApi,
-  websitePurposeCache: WebsitePurposeCache;
-  webpageSummaryApi: WebpageSummaryApi,
+export interface Repositories {
+  getAllBlogPosts: GetAllBlogPosts;
+  getBio: GetBio;
+  getBlogPost: GetBlogPost;
+  getWebpageSummary: GetWebpageSummary;
+  getWebsitePurpose: GetWebsitePurpose;
 }
 
 export const RepositoryContext = React.createContext<Repositories>(null as any);
