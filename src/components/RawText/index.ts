86c709591bd6cf416ac71845e9126e5a547e6ef1
Kohei Asai <yo@kohei.dev>
2020-02-13 17:46:47 -0800

    Fix import with temporary hack

--- src/components/RawText/index.ts
+++ src/components/RawText/index.ts
@@ -1,9 +1,16 @@
 export { default as ThemedColor } from "../../types/ThemedColor";
 export { default } from "./RawText";
-export type { Props as RawTextProps } from "./RawText";
 export { default as RawTextThemeContext } from "./RawTextThemeContext";
-export type { RawTextTheme } from "./RawTextThemeContext";
 export { default as TextAlignment } from "./TextAlignment";
 export { default as TextLineSize } from "./TextLineSize";
 export { default as TextSize } from "./TextSize";
 export { default as Typeface } from "./Typeface";
+
+// TODO:
+// temporary implementation
+// use `export type { ... } from "";` to export types when --isolatedModules is enabled
+// but @babel/preset-typescript doesn't support this syntax yet
+import { Props as _RawTextProps } from "./RawText";
+export type RawTextProps = _RawTextProps;
+import { RawTextTheme as _RawTextTheme } from "./RawTextThemeContext";
+export type RawTextTheme = _RawTextTheme;
