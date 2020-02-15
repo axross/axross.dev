2620a64fa90f9901b81fb72874bb93ab4c994965
Kohei Asai <yo@kohei.dev>
2020-02-14 22:43:29 -0800

    Modify color constants to simple values

--- src/constant/color.ts
+++ src/constant/color.ts
@@ -1,73 +1,43 @@
-import ThemedColor from "../types/ThemedColor";
+export const LIGHT_FOREGROUND_COLOR = "#222f3e"
+export const LIGHT_EMPHASIZED_FOREGROUND_COLOR = "#11181f"
+export const LIGHT_SECONDARY_FOREGROUND_COLOR = "#8395a7"
+export const LIGHT_PRIMARY_FOREGROUND_COLOR = "#2e86de"
+export const LIGHT_ACCENT_FOREGROUND_COLOR = "#ff9f43"
+export const LIGHT_WHITE_FOREGROUND_COLOR = "#ffffff"
+export const LIGHT_BACKGROUND_COLOR = "#ffffff"
+export const LIGHT_SECONDARY_BACKGROUND_COLOR = "#f8f9fa"
+export const LIGHT_ACCENT_BACKGROUND_COLOR = "#FFF2D5"
+export const LIGHT_LOADER_COLOR = "#E0E4E9"
+export const LIGHT_LOADER_HIGHLIGHT_COLOR = "#EFF2F4"
 
-type ColorTheme = Record<ThemedColor, string>;
+export const DARK_FOREGROUND_COLOR = "#eee7e0";
+export const DARK_EMPHASIZED_FOREGROUND_COLOR = "#ffffff";
+export const DARK_SECONDARY_FOREGROUND_COLOR = "#8395a7";
+export const DARK_PRIMARY_FOREGROUND_COLOR = "#54a0ff";
+export const DARK_ACCENT_FOREGROUND_COLOR = "#feca57";
+export const DARK_WHITE_FOREGROUND_COLOR = "#ffffff";
+export const DARK_BACKGROUND_COLOR = "#11181f";
+export const DARK_SECONDARY_BACKGROUND_COLOR = "#172026";
+export const DARK_ACCENT_BACKGROUND_COLOR = "#4C442D";
+export const DARK_LOADER_COLOR = "#1E2730";
+export const DARK_LOADER_HIGHLIGHT_COLOR = "#2D3641";
 
-export const LIGHT_COLOR: ColorTheme = {
-  [ThemedColor.foreground]: "#222f3e",
-  [ThemedColor.emphasizedForeground]: "#11181f",
-  [ThemedColor.secondaryForeground]: "#8395a7",
-  [ThemedColor.primaryForeground]: "#2e86de",
-  [ThemedColor.accentForeground]: "#ff9f43",
-  [ThemedColor.whiteForeground]: "#ffffff",
-  [ThemedColor.background]: "#ffffff",
-  [ThemedColor.secondaryBackground]: "#f8f9fa",
-  [ThemedColor.accentBackground]: "#FFF2D5",
-  [ThemedColor.loader]: "#E0E4E9",
-  [ThemedColor.loaderHighlight]: "#EFF2F4"
-};
+export const SYNTAX_HIGHLIGHT_BACKGROUND_COLOR = "#222f3e";
 
-export const DARK_COLOR: ColorTheme = {
-  [ThemedColor.foreground]: "#eee7e0",
-  [ThemedColor.emphasizedForeground]: "#ffffff",
-  [ThemedColor.secondaryForeground]: "#8395a7",
-  [ThemedColor.primaryForeground]: "#54a0ff",
-  [ThemedColor.accentForeground]: "#feca57",
-  [ThemedColor.whiteForeground]: "#ffffff",
-  [ThemedColor.background]: "#11181f",
-  [ThemedColor.secondaryBackground]: "#172026",
-  [ThemedColor.accentBackground]: "#4C442D",
-  [ThemedColor.loader]: "#1E2730",
-  [ThemedColor.loaderHighlight]: "#2D3641"
-};
-
-export enum CodeColor {
-  normal,
-  comment,
-  punctuation,
-  keyword,
-  operator,
-  function,
-  string,
-  number,
-  class,
-  tag,
-  attributeKey,
-  attributeValue,
-  constant,
-  parameter,
-  property,
-  selector,
-  hexcode
-}
-
-export const CODE_BACKGROUND_COLOR = "#222f3e";
-
-export const CODE_COLORS: Record<CodeColor, string> = {
-  [CodeColor.normal]: "#e06c75",
-  [CodeColor.comment]: "#5c6370",
-  [CodeColor.punctuation]: "#abb2bf",
-  [CodeColor.keyword]: "#c678dd",
-  [CodeColor.operator]: "#56b6c2",
-  [CodeColor.function]: "#56b6c2",
-  [CodeColor.string]: "#98c379",
-  [CodeColor.number]: "#d19a66",
-  [CodeColor.class]: "#e5c07b",
-  [CodeColor.tag]: "#f07178",
-  [CodeColor.attributeKey]: "#c792ea",
-  [CodeColor.attributeValue]: "#61afef",
-  [CodeColor.constant]: "#d19a66",
-  [CodeColor.property]: "#abb2bf",
-  [CodeColor.parameter]: "#abb2bf",
-  [CodeColor.selector]: "#e06c75",
-  [CodeColor.hexcode]: "#56b6c2"
-};
+export const SYNTAX_HIGHLIGHT_NORMAL_COLOR = "#e06c75";
+export const SYNTAX_HIGHLIGHT_COMMENT_COLOR = "#5c6370";
+export const SYNTAX_HIGHLIGHT_PUNCTUATION_COLOR = "#abb2bf";
+export const SYNTAX_HIGHLIGHT_KEYWORD_COLOR = "#c678dd";
+export const SYNTAX_HIGHLIGHT_OPERATOR_COLOR = "#56b6c2";
+export const SYNTAX_HIGHLIGHT_FUNCTION_COLOR = "#56b6c2";
+export const SYNTAX_HIGHLIGHT_STRING_COLOR = "#98c379";
+export const SYNTAX_HIGHLIGHT_NUMBER_COLOR = "#d19a66";
+export const SYNTAX_HIGHLIGHT_CLASS_COLOR = "#e5c07b";
+export const SYNTAX_HIGHLIGHT_TAG_COLOR = "#f07178";
+export const SYNTAX_HIGHLIGHT_ATTRIBUTE_KEY_COLOR = "#c792ea";
+export const SYNTAX_HIGHLIGHT_ATTRIBUTE_VALUE_COLOR = "#61afef";
+export const SYNTAX_HIGHLIGHT_CONSTANT_COLOR = "#d19a66";
+export const SYNTAX_HIGHLIGHT_PROPERTY_COLOR = "#abb2bf";
+export const SYNTAX_HIGHLIGHT_PARAMETER_COLOR = "#abb2bf";
+export const SYNTAX_HIGHLIGHT_SELECTOR_COLOR = "#e06c75";
+export const SYNTAX_HIGHLIGHT_HEXCODE_COLOR = "#56b6c2";
