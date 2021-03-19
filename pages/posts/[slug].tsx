929fdd4839c611e93397176ae488ac8e76706ea3
Kohei Asai <yo@kohei.dev>
2021-03-18 22:20:30 -0700

    Return cache header

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -381,6 +381,7 @@ const Page: React.VFC<PageProps> = (props) => {
 
 export async function getServerSideProps({
   req,
+  res,
   query,
   params,
 }: GetServerSidePropsContext) {
@@ -397,6 +398,11 @@ export async function getServerSideProps({
   const intlMessages = await getIntlMessages({ locale });
   const posts = await getPostEntryListJson({ locale });
   const post = await getPostJson({ slug, locale });
+
+  res.setHeader(
+    "cache-control",
+    "max-age=60, stale-while-revalidate=3600, public"
+  );
 
   if (post === null) {
     return {
