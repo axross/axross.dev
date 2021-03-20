37eb1adb6fc9a387db2ace5b1953095beed142f4
Kohei Asai <yo@kohei.dev>
2021-03-19 19:06:02 -0700

    Install google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,30 @@ class Document extends NextDocument {
   render() {
     return (
       <Html lang={this.props.__NEXT_DATA__.props.pageProps.locale}>
-        <Head />
+        <Head>
+          {/*
+           * initialize google analytics only when the env var is set
+           * you can comment out the env var in .env.local when you want to debug
+           **/}
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID ? (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', { send_page_view: false });
+                  `,
+                }}
+              />
+            </>
+          ) : null}
+        </Head>
 
         <body>
           <Main />
