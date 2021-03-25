99ca460cc50fce3ba366fec5f8176b1873a9d353
Kohei Asai <yo@kohei.dev>
2021-03-24 20:45:23 -0700

    feat(component): add `<Button>` component (#273)

---
+++ components/button.stories.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Mic } from "react-feather";
+import { Story, Meta } from "@storybook/react";
+
+import { Button, ButtonProps, ButtonSize, ButtonVariant } from "./button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: [ButtonVariant.default, ButtonVariant.inverted],
+      },
+    },
+    size: {
+      control: {
+        type: "select",
+        options: [ButtonSize.sm, ButtonSize.md, ButtonSize.lg, ButtonSize.xl],
+      },
+    },
+    icon: {
+      control: false,
+    },
+  },
+  args: {
+    variant: ButtonVariant.default,
+    size: ButtonSize.md,
+    children: "Click me",
+  },
+} as Meta;
+
+export const Basic: Story<ButtonProps> = (props) => <Button {...props} />;
+
+export const WithIcon = Basic.bind({});
+WithIcon.args = {
+  icon: <Mic />,
+};
+
+export const HugeIcon = Basic.bind({});
+HugeIcon.args = {
+  size: ButtonSize.xl,
+  icon: <Mic />,
+  children: null,
+};
