23c5d03affe86395ff3697ca5186396ddeff6940
Kohei Asai <yo@kohei.dev>
2021-03-26 23:36:09 -0700

    Organize test setup files

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,7 +2,7 @@ const baseJestConfig = require("./jest.config");
 
 module.exports = {
   ...baseJestConfig,
-  globalSetup: "<rootDir>/e2e-global-setup.js",
+  globalSetup: "<rootDir>/test-setup/e2e-global-setup.js",
   testMatch: ["<rootDir>/e2e/?(*.)+(spec).ts?(x)"],
   testTimeout: 30000,
 };
