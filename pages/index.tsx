a50458bb1690744395f91dad21e7e0f0cb486080
Kohei Asai <yo@kohei.dev>
2021-05-02 17:49:49 -0700

    feat(app): migrative redirection

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,21 @@ import { getDefaultLocale, getLocales } from "../helpers/localization";
 const Page: NextPage = () => null;
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (req.url?.includes("?")) {
+    const searchParams = new URL(req.url, process.env.NEXT_PUBLIC_SELF_ORIGIN)
+      .searchParams;
+    const requestedLocale = getLocales().find(
+      (locale) => locale === searchParams.get("hl")?.toLowerCase()
+    );
+
+    return {
+      redirect: {
+        permanent: true,
+        destination: `/${requestedLocale}`,
+      },
+    };
+  }
+
   const negotiator = new Negotiator(req);
   const locale = negotiator.language(getLocales()) ?? getDefaultLocale();
 
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -8,6 +8,21 @@ export const getServerSideProps: GetServerSideProps<
   {},
   { slug: string }
 > = async ({ req, params }) => {
+  if (req.url?.includes("?")) {
+    const searchParams = new URL(req.url, process.env.NEXT_PUBLIC_SELF_ORIGIN)
+      .searchParams;
+    const requestedLocale = getLocales().find(
+      (locale) => locale === searchParams.get("hl")?.toLowerCase()
+    );
+
+    return {
+      redirect: {
+        permanent: true,
+        destination: `/${requestedLocale}/posts/${params!.slug}`,
+      },
+    };
+  }
+
   const negotiator = new Negotiator(req);
   const locale = negotiator.language(getLocales()) ?? getDefaultLocale();
 
