0e0a43444a5af5590106f7ecdd4a02112ec1916b
Kohei Asai <yo@kohei.dev>
2021-05-09 12:35:11 -0700

    feat: reduce http requests to post content github repo

--- adapters/post-repository.ts
+++ adapters/post-repository.ts
@@ -1,4 +1,3 @@
-import { Octokit } from "@octokit/core";
 import matter from "gray-matter";
 import {
   generateMarkdown,
@@ -8,84 +7,30 @@ import {
 
 const owner = process.env.GITHUB_CONTENT_REPO!.split("/")[0]!;
 const repo = process.env.GITHUB_CONTENT_REPO!.split("/")[1]!;
-const postPathRegexp = /^posts\/([a-z-]+)\/([a-z]{2}-[a-z]{2}).md$/;
-
-async function getDefaultBranchName(): Promise<string> {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_ACCESS_TOKEN,
-  });
-
-  try {
-    const {
-      data: { default_branch },
-    } = await octokit.request("GET /repos/{owner}/{repo}", {
-      owner: "axross",
-      repo: "posts",
-    });
-
-    return default_branch;
-  } catch (error) {
-    console.error("send the error to sentry");
-
-    throw new Error();
-  }
-}
+const ref = process.env.GITHUB_CONTENT_REF;
 
 export async function getAllPosts({
-  locale: requestedLocale,
+  locale,
 }: {
-  locale?: string;
+  locale: string;
 }): Promise<
   {
     slug: string;
     title: string;
-    description: string;
-    coverImageUrl: string;
-    tags: string[];
-    firstPublishedAt: string;
     lastPublishedAt: string;
-    tableOfContents: { id: string; level: number; text: string }[];
-    body: string;
   }[]
 > {
-  const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-  const defaultBranch = await getDefaultBranchName();
-  const {
-    data: { tree },
-  } = await octokit.request("GET /repos/{owner}/{repo}/git/trees/{tree_sha}", {
-    owner,
-    repo,
-    tree_sha: defaultBranch,
-    recursive: "true",
-  });
-
-  return await Promise.all(
-    tree
-      .filter((node) => {
-        if (node.type !== "blob") {
-          return false;
-        }
-
-        const match = node.path!.match(postPathRegexp);
-
-        if (!match) {
-          return false;
-        }
-
-        if (match[2]! !== requestedLocale) {
-          return false;
-        }
-
-        return true;
-      })
-      .map(async (node) => {
-        const match = node.path!.match(postPathRegexp);
-        const slug = match![1]!;
-        const locale = match![2]!;
-
-        return (await getPost({ slug, locale }))!;
-      })
+  const response = await fetch(
+    `https://raw.githubusercontent.com/${owner}/${repo}/${ref}/${locale}.json`
   );
+
+  if (response.status !== 200) {
+    return [];
+  }
+
+  const posts = await response.json();
+
+  return posts.map((post: any) => ({ ...post, slug: post.id }));
 }
 
 export async function getPost({
@@ -105,9 +50,8 @@ export async function getPost({
   tableOfContents: { id: string; level: number; text: string }[];
   body: string;
 } | null> {
-  const defaultBranch = await getDefaultBranchName();
   const response = await fetch(
-    `https://raw.githubusercontent.com/${owner}/${repo}/${defaultBranch}/posts/${slug}/${locale}.md`
+    `https://raw.githubusercontent.com/${owner}/${repo}/${ref}/posts/${slug}/${locale}.md`
   );
 
   if (response.status !== 200) {
